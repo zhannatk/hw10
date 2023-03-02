9285c216fd5eb1ee98a1f0720089713b04a8b920
Жанна <zhanna.kindgirl@gmail.com>
2023-03-02 13:29:29 +0300

    hw edit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,109 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+
+
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] weights = new int[2];
+        weights[0] = 1;
+        weights[1] = 2;
+        weights[2] = 3;
+        double[] weight = {1.57, 7.654, 9.986};
+        int[] redBox = new int[4];
+        redBox[0] = 12;
+        int books = redBox[0];
+        redBox[1] = 1;
+        int fails = redBox[1];
+        redBox[2] = 22;
+        int  pens = redBox[2];
+        redBox[3] = 43;
+        int glass = redBox[3];
+        redBox[4] = 4;
+        int forms = redBox[4];
+        redBox[5] = 222;
+        int pencil = redBox[5];
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] weights = new int[2];
+        weights[0] = 1;
+        weights[1] = 2;
+        weights[2] = 3;
+        double[] weight = {1.57, 7.654, 9.986};
+        int[] redBox = new int[4];
+        redBox[0] = 12;
+        int books = redBox[0];
+        redBox[1] = 1;
+        int fails = redBox[1];
+        redBox[2] = 22;
+        int  pens = redBox[2];
+        redBox[3] = 43;
+        int glass = redBox[3];
+        redBox[4] = 4;
+        int forms = redBox[4];
+        redBox[5] = 222;
+        int pencil = redBox[5];
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+    }
+
+    public static void task9() {
+        System.out.println("Задача 9");
+    }
+
+    public static void task10() {
+        System.out.println("Задача 10");
+        int[] weights = {90, 91, 92, 93, 94, 95, 0, 0, 0, 0};
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        System.out.println(weights[2]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+
+        }
+    }
+
+
 }
